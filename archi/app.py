fa203630d63bdddc0092866ab676b4ae0f8ff9cb
Artsiom Sinitski <artsiom.vs@gmail.com>
2024-08-29 19:54:15 +0000

    added topics widget

--- archi/app.py
+++ archi/app.py
@@ -27,14 +27,12 @@ PAGE_TITLE: str = "Virtual Legal Assistant"
 # LANG_EN: str = "EN"
 # LANG_RU: str = "RU"dir
 
-st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+# ==== Controls setup =========================================
 
-# with st.sidebar:
-#     pass
-    # openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    # "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    # "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    # "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+# ch_inp = st.chat_input(key="chatInput_1", disabled=True)
+sh_inp = st.chat_input(key="MaincChatInput", disabled=True)
+# ==== End Controls setup =====================================
 
 header = st.columns(2)
 # ------------------------
@@ -43,61 +41,92 @@ header_tile0.image("archi/content/img/attorney3.jpg", width=260)
 # ------------------------
 header_tile1 = header[1].container(height=None, border=False)
 
-header_tile1.write("💬 Арчия к Вашим услугам! Она может:")
-header_tile1.divider()
+header_tile1.write("💬 Здравствуйте! Меня зовут Арчия")
+header_tile1.write("💬 Я - виртуальный юрист, который может:")
+# header_tile1.divider()
 header_tile1.markdown("* ответить на Ваши вопросы")
 header_tile1.markdown("* предоставить нужные бланки")
 header_tile1.markdown("* дать понятные пошаговые инструкции")
 
 st.divider()
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Чем я могу Вам помочь?"}]
+# with st.sidebar:
+#     topic: tuple[str] = st.selectbox(
+#         "Выберите тему вопроса:",
+#         ("Расчитай неустойку", "Ответь на вопрос", "Сделай справку", "Резюмируй", "Переведи")
+#     ),
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+    # pass
+    # openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    # "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    # "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    # "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+# with end
 
-if user_input := st.chat_input():
-    try:
-        openai_api_key = st.secrets.api_credentials.api_key
-    except (KeyError, AttributeError) as err:
-        # pass
-        # st.error(st.session_state.locale.empty_api_handler)
-        logging.info(str(err))
+with st.container():
+    # st.write('test')
+    topic: tuple[str] = st.selectbox("Выберите тему вопроса:",
+            ("...", "Расчитай неустойку", "Ответь на вопрос", "Сделай справку", "Резюмируй", "Переведи"),
+            label_visibility="visible",
+            placeholder="..."
+        ),
+    # md_txt: str = f"**Выбранная тема:**\t{topic[0]}"
+    # st.markdown(md_txt)
+# with end
+st.divider()
 
-    if not openai_api_key:
-        st.info("Your API key is missing or invalid!")
-        st.stop()
+if topic != "...":          # enable chat input widget only if topic selected
+    ch_inp = st.session_state["MaincChatInput"]  #(disabled=False)
+    st.write(ch_inp)
+    # st.session_state['1'](disabled=False)
 
-    client = OpenAI(api_key=openai_api_key)
-    PROMPT = PromptTemplate(template=sys_prompt_ru_1, input_variables=["context", "question"])
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant", "content": "Какой Ваш конкретный вопрос?"}]
 
-    llm = ChatOpenAI(
-        api_key=openai_api_key,
-        temperature=0,
-        model=llm_model
-    )
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
 
-    # create the chain to answer questions
-    qa_chain = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=dp.get_vector_db_retriever(),
-        return_source_documents=True,
-        chain_type_kwargs={"prompt": PROMPT}
-    )
+    if user_input := ch_inp:
+        try:
+            openai_api_key = st.secrets.api_credentials.api_key
+        except (KeyError, AttributeError) as err:
+            # pass
+            # st.error(st.session_state.locale.empty_api_handler)
+            logging.info(str(err))
 
-    st.session_state.messages.append({"role": "user", "content": user_input})
-    st.chat_message("user").write(user_input)
+        if not openai_api_key:
+            st.info("Your API key is missing or invalid!")
+            st.stop()
 
-    # response = client.chat.completions.create(model=llm_model, messages=st.session_state.messages)
-    # assistant_response = response.choices[0].message.content
+        client = OpenAI(api_key=openai_api_key)
+        PROMPT = PromptTemplate(template=sys_prompt_ru_1, input_variables=["context", "question"])
 
-    response = qa_chain(user_input)
-    assistant_response = response.get("result")
+        llm = ChatOpenAI(
+            api_key=openai_api_key,
+            temperature=0,
+            model=llm_model
+        )
 
-    st.session_state.messages.append({"role": "assistant", "content": assistant_response})
-    st.chat_message("assistant").write(assistant_response)
+        # create the chain to answer questions
+        qa_chain = RetrievalQA.from_chain_type(
+            llm=llm,
+            chain_type="stuff",
+            retriever=dp.get_vector_db_retriever(),
+            return_source_documents=True,
+            chain_type_kwargs={"prompt": PROMPT}
+        )
+
+        st.session_state.messages.append({"role": "user", "content": user_input})
+        st.chat_message("user").write(user_input)
+
+        # response = client.chat.completions.create(model=llm_model, messages=st.session_state.messages)
+        # assistant_response = response.choices[0].message.content
+
+        response = qa_chain(user_input)
+        assistant_response = response.get("result")
+
+        st.session_state.messages.append({"role": "assistant", "content": assistant_response})
+        st.chat_message("assistant").write(assistant_response)
 
 # if __name__ == "__main__":
 #     pass
