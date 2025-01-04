cf90c3704a0319efe04c0d4026a4a1a0d8caae90
Artsiom_Sinitski <Artsiom.Sinitski@burberry.com>
2025-01-04 21:44:54 +0100

    dev-TEST: added llm_model to out file

--- archi/tests/test_Q_and_A.py
+++ archi/tests/test_Q_and_A.py
@@ -15,16 +15,10 @@ from archi.src.constants import (WIN_ENCODING_RU, AI_MODELS)
 from archi.src.prompts import sys_prompt_to_calculate_penalty_ru
 import archi.src.docs_processor as dp
 
-# --------------------------------------
-openai_api_key: str = os.environ["OPENAI_API_KEY"]
-llm_model: str = AI_MODELS.get("gpt4-o1")
-# --------------------------------------
-
 
 def main() -> None:
-    start_time: float = 0.0
-    end_time: float = 0.0
-    elapsed_time: str = ""
+    openai_api_key: str = os.environ["OPENAI_API_KEY"]
+    llm_model: str = AI_MODELS.get("gpt4-o1")
     curr_date: datetime = datetime.now()
 
     input_file_path: str = rf"{os.environ['USERDIR']}\Documents\archi_knowledge_docs\test_q_and_a\curr_Q-file"
@@ -57,8 +51,8 @@ def main() -> None:
     )
     # ---------------------------------------------------------------------------------
 
-    with open(fr"{full_q_file_path}", 'r', encoding=WIN_ENCODING_RU) as fp:
-        data = json.load(fp)
+    with open(fr"{full_q_file_path}", 'r', encoding=WIN_ENCODING_RU) as in_fp:
+        data = json.load(in_fp)
 
     questions = data.get("Questions")
 
@@ -66,28 +60,32 @@ def main() -> None:
     files: list[str] = os.listdir(dp.knowledge_docs_path)
     files = sorted([fi for fi in files if os.path.isfile(dp.knowledge_docs_path + '/' + fi)])
 
-    with open(fr"{output_file_path}\{ans_file_name}", 'w', encoding="utf-8") as fp:
-        fp.write(f"     DATE:\t{curr_date.strftime('%Y-%m-%d %H:%M')}\n")
-        fp.write(f" ENCODING:\t{WIN_ENCODING_RU}\n")
-        fp.write(f"   Q-FILE:\t{q_file_name[0]}\n")
-        fp.write(f"     DOCS:\t{len(files)}\n")
-        fp.writelines([f"\t - {file}\n" for file in files])
-        fp.write(f"\n   PROMPT:{sys_prompt_to_calculate_penalty_ru}")
-        fp.write(f"\n{'#'*70}\n")
+    with open(fr"{output_file_path}\{ans_file_name}", 'w', encoding="utf-8") as out_fp:
+        out_fp.write(f"DATE:\t{curr_date.strftime('%Y-%m-%d %H:%M')}\n")
+        out_fp.write(f"LLM MODEL:\t{llm_model}\n")
+        out_fp.write(f"ENCODING:\t{WIN_ENCODING_RU}\n")
+        out_fp.write(f"Q-FILE:\t{q_file_name[0]}\n")
+        out_fp.write(f"DOCS:\t{len(files)}\n")
+        out_fp.writelines([f"\t - {file}\n" for file in files])
+        out_fp.write(f"\nPROMPT:{sys_prompt_to_calculate_penalty_ru}")
+        out_fp.write(f"\n{'#'*70}\n")
 
         print(f"{'*'*3} Started processing questions...")
+        start_time: float = 0.0
+        end_time: float = 0.0
+        elapsed_time: str = ""
         for idx, question in enumerate(questions, start=1):
             start_time = time.perf_counter()
             response = qa_chain(question)
             end_time = time.perf_counter() - start_time
             elapsed_time = time.strftime("%H:%M:%S", time.gmtime(end_time))
 
-            fp.writelines([f"{elapsed_time}\nQ{idx} - \n", f"{'-'*15}\n", f"{question}\n\n", "Answer:\n"])
+            out_fp.writelines([f"{elapsed_time}\nQ{idx} - \n", f"{'-'*15}\n", f"{question}\n\n", "Answer:\n"])
             try:
-                fp.write(response.get("result"))
+                out_fp.write(response.get("result"))
             except Exception as ex:
                 print(f"\t{'*'*3} {ex}")
-            fp.write(f"\n\n{'-'*99}\n")
+            out_fp.write(f"\n\n{'-'*99}\n")
             print(f"\tProcessed Q{idx} ({elapsed_time}) -> {question}")
         # for end
         print(f"{'*' * 3} Finished processing questions {'*' * 3}")
