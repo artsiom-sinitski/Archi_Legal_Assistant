54825e0944ee72edd39689bf64a36ac47be99e08
Artsiom_Sinitski <Artsiom.Sinitski@burberry.com>
2025-06-28 22:36:10 +0200

    dev-TEST::added langgraph file

---
+++ AiLita/src/workflow_graph.py
@@ -0,0 +1,18 @@
+from typing import Annotated
+
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+
+
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+llm = init_chat_model("openai:gpt-4.1")
